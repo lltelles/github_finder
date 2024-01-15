755d5323e28372b627864c1cad01af524822f974
Lucas Telles <lltellesss@gmail.com>
2024-01-15 19:37:10 -0300

    added search component to home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+import Search from "../components/Search";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
 
 export default Home;
