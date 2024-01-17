a4050fb7dc89071220d4da2002d4e9de22ef176a
Lucas Telles <lltellesss@gmail.com>
2024-01-16 22:45:55 -0300

    added list of repos with all the infos

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -8,6 +8,7 @@ import classes from "./Repos.module.css";
 
 import BackBtn from "../components/BackBtn";
 import Loader from "../components/Loader";
+import Repo from "../components/Repo";
 
 const Repos = () => {
   const { username } = useParams();
@@ -43,7 +44,7 @@ const Repos = () => {
       {repos && repos.length > 0 && (
         <div>
           {repos.map((repo: RepoProps) => (
-            <p>{repo.name}</p>
+            <Repo key={repo.name} {...repo} />
           ))}
         </div>
       )}
