1d7776fb293752a5476a455f25ab19f528e37d8d
Lucas Telles <lltellesss@gmail.com>
2024-01-16 22:25:20 -0300

    added user repo fetch

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -1,12 +1,34 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import { RepoProps } from "../types/repo";
+
 import classes from "./Repos.module.css";
 
 import BackBtn from "../components/BackBtn";
+import Loader from "../components/Loader";
 
 const Repos = () => {
   const { username } = useParams();
+
+  const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadRepos = async function (username: string) {
+      setIsLoading(true);
+
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+      const data = await res.json();
+
+      setIsLoading(false);
+
+      console.log(data);
+    };
+    loadRepos(username);
+  }, []);
 
   return (
     <div>
